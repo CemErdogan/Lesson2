eb4f81bc49de481762e9fd82eec41618c6f4a3e8
Cem Erdoğan <cem.cemerdogan97@gmail.com>
2024-07-09 21:21:10 +0300

    find button

--- Assets/Scripts/Runtime/UI/LevelsButton.cs
+++ Assets/Scripts/Runtime/UI/LevelsButton.cs
@@ -1,11 +1,21 @@
 using DG.Tweening;
+using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelsButton : MonoBehaviour
 {
+    [SerializeField] Button button;
+    
     void OnEnable()
     {
         ClickMeAnimation();
+        button.onClick.AddListener(OnClick);
+    }
+
+    void OnDisable()
+    {
+        button.onClick.RemoveListener(OnClick);
     }
 
     void ClickMeAnimation()
@@ -20,4 +30,18 @@ public class LevelsButton : MonoBehaviour
             })
             .SetId(transform);
     }
+
+    void OnClick()
+    {
+        
+    }
+
+#if UNITY_EDITOR
+    [Button]
+    void FindButton()
+    {
+        button = GetComponent<Button>();
+    }
+#endif
+
 }
