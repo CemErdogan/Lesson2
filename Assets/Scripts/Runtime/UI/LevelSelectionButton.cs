a1f7dc6d3ba41aefa7981ce4c84328234800f045
Cem Erdoğan <cem.cemerdogan97@gmail.com>
2024-07-10 22:00:30 +0300

    Updated level selection button

--- Assets/Scripts/Runtime/UI/LevelSelectionButton.cs
+++ Assets/Scripts/Runtime/UI/LevelSelectionButton.cs
@@ -1,9 +1,31 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-namespace Runtime.UI
+public class LevelSelectionButton : MonoBehaviour
 {
-    public class LevelSelectionButton : MonoBehaviour
+    [SerializeField] GameObject lockIcon, playText;
+    [SerializeField] TextMeshProUGUI levelInfoText;
+    [SerializeField] Button playButton;
+    int _index;
+
+    public void Prepare(LevelScoresData data)
     {
-        
+        playButton.interactable = data.isUnlocked;
+        _index = data.index;
+        UpdateSprite(data.isUnlocked);
+        UpdateInfoText(data);
+    }
+
+    void UpdateSprite(bool isUnlocked)
+    {
+        lockIcon.SetActive(!isUnlocked);
+        playText.SetActive(isUnlocked);
+    }
+
+    void UpdateInfoText(LevelScoresData data)
+    {
+        levelInfoText.text = $"Level {data.index} - {data.title} {Environment.NewLine} HighScore: {data.highScore}";
     }
 }
