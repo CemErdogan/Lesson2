1a63f3434a9e4706aaea41607e1ca60aaef6f3ba
cem.erdogan <cem.erdogan@rovio.com>
2024-07-22 23:21:59 +0300

    camera adjusment

--- Assets/Scripts/Runtime/Managers/CameraHandler.cs
+++ Assets/Scripts/Runtime/Managers/CameraHandler.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Collections;
+using System.Linq;
 using Cinemachine;
 using UnityEngine;
 
@@ -8,10 +9,18 @@ public class CameraHandler : MonoBehaviour
     [SerializeField] SubmitManager submitManager;
     [SerializeField] Transform tileParent, blockParent;
     [SerializeField] float offset = 20;
+    [SerializeField] float offsetWeight = 1;
 
     void Awake()
     {
         GameEvents.OnTilesSpawned += Prepare;
+    }
+
+    IEnumerator Start()
+    {
+        yield return null;
+
+        blockParent.position = new Vector3(GetMeanTileX(), GetTopTileY() + offset, GetMeanTileZ());
     }
 
     void OnDestroy()
@@ -27,15 +36,45 @@ public class CameraHandler : MonoBehaviour
         for (int i = 0; i < tiles.Length; i++)
         {
             targetGroup.m_Targets[i].target = tiles[i].transform;
-            targetGroup.m_Targets[i].weight = 1;
+            targetGroup.m_Targets[i].weight = offsetWeight;
         }
         
         var targetIndex = tiles.Length;
         foreach (var block in submitManager.SubmitBlocks)
         {
             targetGroup.m_Targets[targetIndex].target = block.transform;
-            targetGroup.m_Targets[targetIndex].weight = 1;
+            targetGroup.m_Targets[targetIndex].weight = offsetWeight;
             targetIndex++;
         }
+    }
+    
+    float GetTopTileY()
+    {
+        return tileParent.Cast<Transform>()
+            .Aggregate(float.MinValue, (current, child) => Mathf.Max(current, child.position.y));
+    }
+    
+    float GetMeanTileX()
+    {
+        var meanX = 0f;
+        
+        foreach (Transform tile in tileParent)
+        {
+            meanX += tile.position.x;
+        }
+
+        return meanX / tileParent.childCount;
+    }
+    
+    float GetMeanTileZ()
+    {
+        var meanZ = 0f;
+        
+        foreach (Transform tile in tileParent)
+        {
+            meanZ += tile.position.z;
+        }
+
+        return meanZ / tileParent.childCount;
     }
 }
