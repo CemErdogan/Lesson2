8647f422e2d30cd2d18e5c3af66d4dd07b669829
cem.erdogan <cem.erdogan@rovio.com>
2024-07-29 22:27:58 +0300

    level win/lose

--- Assets/Scripts/Runtime/Gameplay/Events/GameEvents.cs
+++ Assets/Scripts/Runtime/Gameplay/Events/GameEvents.cs
@@ -2,6 +2,8 @@ using System;
 
 public static class GameEvents
 {
+    public static Action OnWin, OnFail;
+    public static Action OnCompleted;
     public static Action<SubmitBlock, string> OnTileAttached;
     public static Action<SubmitBlock> OnTileRemoved;
     public static Action<Tile[]> OnTilesSpawned;
